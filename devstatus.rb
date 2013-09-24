fec5ad0e04144354dea924bd13f32fd8b8ef18ab
cassiuschen <chzsh1995@gmail.com>
2013-09-24 13:31:23 +0800

    Finished Ruby Script ver.1

--- devstatus.rb
+++ devstatus.rb
@@ -2,21 +2,20 @@ filename = dev.txt
 file = open(filename)
 while text = file.gets |
 	contain = %w(text)
-	printtable(contain.shift)
-	
-	
+	while input = contain.shift
+		printtable(input)
+	end
 end
 
 def printtable(element)
 	case element
-	when 'T'
-		
+	when "T","t","ok","running","true"
+		print '<td><i class="uk-icon-ok"></i></td>'
+	when "F","f","false","wrong","problem"
+		print '<td><i class="uk-icon-remove"></i></td>'
+	when "-"
+		print '<td></td>'
+	else
+		print '<td>' , element , '</td>'		
 	end
-	if element == 'T'
-		print '<td><i class="uk-icon-ok"></i><td>'
-	elsif element == 'F'
-		print '<td><i class="uk-icon-ok"></i><td>'
-		
-	end
-	print '<td>',element,'<td>'
 end
