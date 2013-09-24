b715462cfd98168b848325c911857d90e8891b74
cassiuschen <chzsh1995@gmail.com>
2013-09-24 16:43:22 +0800

    change layout

--- data.rb
+++ data.rb
@@ -1,23 +1,23 @@
 $file_url = {
 	"index" => "index.html",
-	"mirrors" => "mirrors.html"
-	"ubuntu" => "http://mirrors.thecmw.cn/ubuntu"
-	"debian" => "http://mirrors.thecmw.cn/debian"
-	"ruby" => "http://rubygems.bdfzer.com"
-	"dev" => "dev.html"
+	"mirrors" => "mirrors.html",
+	"ubuntu" => "http://mirrors.thecmw.cn/ubuntu",
+	"debian" => "http://mirrors.thecmw.cn/debian",
+	"ruby" => "http://rubygems.bdfzer.com",
+	"dev" => "dev.html",
 	"cloud" => "http://cloud.thecmw.cn"
 }
 
 $saas_production = {
-	"bdfzer" => "http://bdfzer.com"
-	"chat" => "http://chat.saas.bdfzer.com"
+	"bdfzer" => "http://bdfzer.com",
+	"chat" => "http://chat.saas.bdfzer.com",
 	"cmj" => "http://media.pkuschool.edu.cn"
 }
 
 $website_info = {
-	"title" => "SaaS"
-	"jsroot" => "../js"
-	"cssroot" => "../css"
+	"title" => "SaaS",
+	"jsroot" => "../js",
+	"cssroot" => "../css",
 	"svgroot" => "../svg"
 }
 
